4cdf884fb84effdde962260ee2f6b57bb32b379f
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-24 11:50:58 -0300

    Implementação do Login e do cadastro de usuário

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { View,  TextInput, StyleSheet, Button } from 'react-native';
+import { 
+    View,  
+    TextInput,
+    StyleSheet, 
+    Button, 
+    Text,
+    ActivityIndicator,
+    Alert
+} from 'react-native';
+import firebase from 'firebase';
 
 import FormRow from '../components/FormRow';
 
@@ -10,7 +19,26 @@ export default class LoginPage extends React.Component {
 
         this.state = {
             mail: '',
-            password: ''
+            password: '',
+            isLoading: false,
+            message: ''
+        }
+    }
+
+    componentDidMount() {
+        const firebaseConfig = {
+            apiKey: "*************",
+            authDomain: "*************",
+            databaseURL: "*************",
+            projectId: "*************",
+            storageBucket: "*************",
+            messagingSenderId: "*************",
+            appId: "*************"
+        };
+
+        // Initialize Firebase
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
         }
     }
 
@@ -34,7 +62,102 @@ export default class LoginPage extends React.Component {
     }
 
     tryLogin() {
-        console.log(this.state);
+        this.setState({
+            isLoading: true,
+            message: ''
+        });
+
+        const { mail, password } = this.state;
+        const loginUserSuccess = user => {
+            this.setState({ message: 'Usuário criado' })
+        }
+        const loginUserFailed = error => {
+            this.setState({
+                message: this.getMessageByErrorCode(error.code)
+            })
+        }
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(
+                mail, 
+                password
+            )
+            .then(loginUserSuccess)
+            .catch(error => {
+
+                if (error.code === 'auth/user-not-found') {
+                    /* Alert.alert(Title, Message, Button[{text, onPress}]) */
+                    Alert.alert(
+                        'Usuário não encontrado',
+                        'Deseja criar um cadastro com as informações inseridas?',
+                        [
+                            { 
+                                text: 'Não',
+                                onPress: () => console.log('Usuário não quer criar conta :('),
+                                style: 'cancel' // Only IOS =(
+                            },
+                            {
+                                text: 'Sim',
+                                onPress: () => {
+                                    firebase
+                                        .auth()
+                                        .createUserWithEmailAndPassword(
+                                            mail, password
+                                        )
+                                        .then(loginUserSuccess)
+                                        .catch(loginUserFailed)
+                                }
+                            }
+                        ],
+                        { cancelable: false }
+                    );
+                    return;
+                }
+                
+                loginUserFailed
+            })
+            .then(() => this.setState({
+                isLoading: false
+            }));
+    }
+
+    getMessageByErrorCode(errorCode) {
+        switch (errorCode) {
+            case 'auth/wrong-password': 
+                return 'Senha incorreta';
+            case 'auth/user-not-found':
+                return 'Usuário não encontrado';
+            default: 
+                return 'Erro ao tentar fazer o login';
+        }
+    }
+
+    renderMessage() {
+        const { message } = this.state;
+
+        if (!message) {
+            return null;
+        }
+
+        return (
+            <View>
+                <Text>{ message }</Text>
+            </View>
+        );
+    }
+
+    renderButton() {
+        if (this.state.isLoading) {
+            return <ActivityIndicator />;
+        }
+
+        return (
+            <Button 
+                title="Entrar"
+                onPress={() => this.tryLogin()}
+            />
+        )
     }
 
     render() {
@@ -57,10 +180,9 @@ export default class LoginPage extends React.Component {
                         onChangeText={value => this.onChangeHandler('password', value)}
                     />
                 </FormRow>
-                <Button 
-                    title="Entrar"
-                    onPress={() => this.tryLogin()}
-                />
+
+                { this.renderButton() }
+                { this.renderMessage() }
             </View>
         );
     }
