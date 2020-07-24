9932c9ed46beb9546db70b732510094d24f2ba26
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-24 10:17:00 -0300

    Atualização do state com valores dos inputs

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,13 +4,44 @@ import { View,  TextInput, StyleSheet } from 'react-native';
 import FormRow from '../components/FormRow';
 
 export default class LoginPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mail: '',
+            password: ''
+        }
+    }
+
+    /* onChangeMail(mail) {
+        this.setState({ mail });
+    } 
+
+    onChangePassword(password) {
+        this.setState({ password });
+    } */
+
+    onChangeHandler(field, value) {
+        /* ES5 - Atribuição de valores quando não se sabe o field no tempo de codificação, apenas no tempo de execução
+        const newState = {};
+        newState[field] = value; */ 
+
+        /* ES6 */
+        this.setState({
+            [field]: value
+        });
+    }
+
     render() {
         return (
             <View>
                 <FormRow>
                     <TextInput 
                         placeholder="exemplo@email.com"
-                        style={styles.input} 
+                        style={styles.input}
+                        value={this.state.mail}
+                        onChangeText={value => this.onChangeHandler('mail', value)}
                     />
                 </FormRow>
                 <FormRow>
@@ -18,6 +49,8 @@ export default class LoginPage extends React.Component {
                         placeholder="******"
                         style={styles.input}
                         secureTextEntry
+                        value={this.state.password}
+                        onChangeText={value => this.onChangeHandler('password', value)}
                     />
                 </FormRow>
             </View>
