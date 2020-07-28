4ccde25f58a86f40dcf715923e22e489475e3ccc
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-28 13:14:52 -0300

    Finalização do layout do card de séries

--- src/components/SerieCard.js
+++ src/components/SerieCard.js
@@ -3,27 +3,79 @@ import {
     View, 
     Text, 
     StyleSheet, 
-    Dimensions 
+    Dimensions,
+    Image
 } from 'react-native';
 
-const SerieCard = ({ serie }) => (
-    <View style={styles.container}>
+const SerieCard = ({ serie, isFirstColumn }) => (
+    <View style={[
+        styles.container,
+        isFirstColumn ? styles.firstColumn : styles.lastColumn
+    ]}>
         <View style={styles.card}>
-            <Text>{`${serie.id} - ${serie.title}`}</Text>
+            <Image 
+                source={{
+                    uri: serie.img
+                }}
+                aspectRatio={1}
+                resizeMode='contain'
+            />
+            <View style={styles.cardTitleWrapper}>
+                <Text style={styles.cardTitle}>{serie.title}</Text>
+            </View>
         </View>
     </View>
 );
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 10,
-        height: 150
+        // Solução 2
+        // flex: 0.5,
+        
+        // Solução 1
+        width: '50%',
+        padding: 5,
+        
+        height: Dimensions.get('window').width / 2,
+
+        // borderWidth: 1,
+        // borderColor: 'red'
     },
     card: {
         flex: 1,
         borderWidth: 1,
-        borderColor: '#c5c5c5'
+
+        // Solução 2
+        // margin: 10
+    },
+    cardTitleWrapper: {
+        backgroundColor: 'black',
+        height: 50,
+
+        position: 'absolute',
+        bottom: 0,
+        opacity: .8,
+
+        width: '100%',
+
+        paddingTop: 10,
+        paddingBottom: 10,
+
+        paddingLeft: 1,
+        paddingRight: 1,
+
+        alignItems: 'center'
+    },
+    cardTitle: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    firstColumn: {
+        paddingLeft: 10
+    },
+    lastColumn: {
+        paddingRight: 10
     }
 });
 
--- src/pages/SeriesPage.js
+++ src/pages/SeriesPage.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 
 import series from '../../series.json';
 
 import SerieCard from '../components/SerieCard';
 
+const isEven = number => number % 2 === 0;
+
 const SeriesPage = props => (
 	<View>
 		<FlatList 
 			data={series}
-			renderItem={({ item }) => (
-				<SerieCard serie={item}/>
+			renderItem={({ item, index }) => (
+				<SerieCard 
+					serie={item}
+					isFirstColumn={isEven(index)}
+				/>
 			)}
 			keyExtractor={item => item.id}
 			numColumns={2}
+			ListHeaderComponent={props => (
+				<View style={styles.marginTop} />
+			)}
+			ListFooterComponent={props => (
+				<View style={styles.marginBottom} />
+			)}
 		/>
 	</View>
 );
 
 const styles = StyleSheet.create({
-
+	marginTop: {
+		marginTop: 5
+	},
+	marginBottom: {
+		marginBottom: 5
+	}
 })
 
 export default SeriesPage;
