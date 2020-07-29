16014d5a0960aafe1789b7626e0af3e30bc513e6
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-29 15:13:47 -0300

    action creator para deletar a série

--- src/actions/seriesActions.js
+++ src/actions/seriesActions.js
@@ -1,6 +1,8 @@
 import firebase from "@firebase/app";
 import "@firebase/database";
 
+import { Alert } from 'react-native';
+
 export const SET_SERIES = 'SET_SERIES';
 const setSeries = series => ({
     type: SET_SERIES,
@@ -19,5 +21,39 @@ export const watchSeries = () => {
                 const action = setSeries(series);
                 dispatch(action);
             });
+    }
+}
+
+export const deleteSerie = serie => {
+    return dispatch => {
+        return new Promise((resolve, reject) => {
+            Alert.alert(
+                'Deletar',
+                `Deseja deletar a série ${serie.title}?`,
+                [
+                    {
+                        text: 'Não',
+                        onPress: () => {
+                            resolve(false); // Para saber se a série foi deletada
+                        },
+                        style: 'cancel' // IOS
+                    },
+                    {
+                        text: 'Sim',
+                        onPress: async () => {
+                            const { currentUser } = firebase.auth();
+
+                            await firebase
+                                .database()
+                                .ref(`/users/${currentUser.uid}/series/${serie.id}`)
+                                .remove();
+
+                            resolve(true);
+                        }
+                    }
+                ],
+                { cancelable: false }
+            )
+        });
     }
 }
