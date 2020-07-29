20d9b0786bdf487d7e77fe275d6e330299d45740
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-29 09:56:18 -0300

    estilização inicial do formulário

--- src/pages/SerieFormPage.js
+++ src/pages/SerieFormPage.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, FlatList, Text } from 'react-native';
+import { 
+    StyleSheet, 
+    View, 
+    Text, 
+    TextInput 
+} from 'react-native';
+
+import FormRow from '../components/FormRow';
 
 const SerieFormPage = props => (
 	<View>
-		<Text>Formulário de Série</Text>
+		<FormRow first>
+            <TextInput 
+                style={styles.input}
+                placeholder="Título"
+                value=""
+                onChangeText={value => console.log(value)}
+            />
+        </FormRow>
 	</View>
 );
 
 const styles = StyleSheet.create({
-	
+	input: {
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 5
+    }
 });
 
 export default SerieFormPage;
