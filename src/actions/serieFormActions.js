71268f3d36fb4a47bdfab140334270d21aaed6b9
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-29 11:42:14 -0300

    inserção inicial das informações no firebase

--- src/actions/serieFormActions.js
+++ src/actions/serieFormActions.js
@@ -1,3 +1,7 @@
+// import firebase from 'firebase';
+import firebase from "@firebase/app";
+import "@firebase/database";
+
 export const SET_FIELD = 'SET_FIELD';
 export const setField = (field, value) => {
     return {
@@ -8,5 +12,16 @@ export const setField = (field, value) => {
 }
 
 export const saveSerie = serie => {
+    const { currentUser } = firebase.auth();
+    const { uid } = currentUser;
+
+    firebase
+        .database()
+        .ref(`/users/${uid}/series`)
+        .push(serie)
+        .then(() => {
+            console.log('Salvo no firebase')
+        });
+        
     console.log('Salvando a série', serie);
 }
