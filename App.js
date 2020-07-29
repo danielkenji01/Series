4ec8b0efc7e64b7ec7ba1f8b96850a9155f40172
danielkenji01 <daniel_kenji_1@hotmail.com>
2020-07-29 12:08:02 -0300

    Ignorando warnings específicos

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+// Desabilitar TODOS os warnings
+// console.disableYellowBox = true;
+
+import { YellowBox } from 'react-native';
 import SeriesApp from './src/SeriesApp';
+
+YellowBox.ignoreWarnings(['Setting a timer for a long period of time']);
 
 export default SeriesApp;
